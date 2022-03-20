6d37ce415620fdef37fd450e1844d50c65153c43
Ruben <19mrs87@gmail.com>
2022-03-20 20:28:24 +0200

    f

---
+++ js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// let number = 4.6;
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const person = "Alex";
+
+// const bool = false;
+
+// console.log(somthing);
+
+// const obj = {
+//     name: "Jon",
+//     age: 25,
+//     isMerried: false
+// };
+// // console.log(obj);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 5, {obj}, []];
+// console.log(arr[4]);
+
+
+// alert('Hello');
+
+// const result = confirm('are you here');
+// console.log(result);
+// const answer = +prompt(" вам есть 18", "18");
+// console.log(answer + 5);
+//  const answers = [];
+//  answers[0] = prompt('Как Ваше имя', '');
+//  answers[1] = prompt('Как Вашf фамилия', '');
+//  answers[2] = prompt('сколько Вам лет', '');
+// console.log(typeof(answers));
+
+// const category = 'tous';
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = 'ivan';
+// alert(`privet ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+ 
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2 + 2 * 2 != '8');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && !isClose);
+
+
